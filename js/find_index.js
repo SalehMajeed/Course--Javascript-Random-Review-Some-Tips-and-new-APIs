4a0c22874a51b68e4801f61a52796b5c0b05b0e3
= <saleh.majeed8@gmail.com>
2020-11-03 10:16:22 +0530

    find index

---
+++ js/find_index.js
@@ -0,0 +1,12 @@
+const people = [
+    {
+        name: 'Anony',
+        occupation: 'Thug',
+    },
+    {
+        name: 'Persona',
+        occupation: 'servivor',
+    },
+];
+
+console.log(people.findIndex((person) => person.name == 'Persona'));

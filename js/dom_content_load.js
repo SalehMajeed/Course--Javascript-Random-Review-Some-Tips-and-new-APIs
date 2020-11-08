530aa678b16c85239196b89a8e010ffceada25bc
= <saleh.majeed8@gmail.com>
2020-11-08 14:39:20 +0530

    domcontentload

---
+++ js/dom_content_load.js
@@ -0,0 +1,5 @@
+if (document.readyState == 'loading')
+    document.addEventListener('DOMContentLoaded', function () {
+        const my_title = document.getElementById('h1');
+        console.log(my_title);
+    });

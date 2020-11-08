22073b49aa3ecff46ea21e17c83aa16f18d53878
= <saleh.majeed8@gmail.com>
2020-11-08 13:23:06 +0530

    fetch api

---
+++ js/fetch_apt.js
@@ -0,0 +1,15 @@
+const params = new URLSearchParams({
+    query: 'dinner',
+    language: 'english',
+});
+
+const url = `login.php?${params.toString()}`;
+
+console.log(url);
+
+fetch('login.php', {
+    method: 'GET',
+    body: params,
+})
+    .then((response) => response.text())
+    .then(console.log());

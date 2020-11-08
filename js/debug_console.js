162fbd7f10099253be062defc15d8a34de1e7a9f
= <saleh.majeed8@gmail.com>
2020-11-08 13:55:12 +0530

    console table

---
+++ js/debug_console.js
@@ -0,0 +1,13 @@
+const a = ['anony', 'ja', 'youtube', 'persona'];
+const o = {
+    name: 'anony',
+    age: 19,
+    has_job: true,
+    hair: {
+        color: 'black',
+        style: 'messy',
+    },
+};
+
+console.table(a);
+console.table(o);

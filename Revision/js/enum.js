88611644dca71e0a05d7b6ee7c835947f559432f
= <saleh.majeed8@gmail.com>
2020-11-10 14:16:43 +0530

    enum in js

---
+++ Revision/js/enum.js
@@ -0,0 +1,22 @@
+const seasons = {
+  SUMMER: "summer",
+  WINTER: "winter",
+  SPRING: "spring",
+  AUTUMN: "autumn",
+};
+
+let season = seasons.SPRING;
+
+switch (season) {
+  case seasons.SUMMER:
+    console.log(season);
+    break;
+  case seasons.WINTER:
+    console.log(season);
+    break;
+  case seasons.SPRING:
+    console.log(season);
+    break;
+  case seasons.AUTUMN:
+    console.log(season);
+}

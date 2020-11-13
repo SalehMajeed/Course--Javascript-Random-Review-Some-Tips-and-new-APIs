0476a67f6eb1af4294ae7236f2b855f2e535d82a
= <saleh.majeed8@gmail.com>
2020-11-13 16:34:17 +0530

    rest operator

---
+++ js/es6_rest_parameter.js
@@ -0,0 +1,14 @@
+function add(a, b, ...nums) {
+  let total = 0;
+
+  console.log(a, b);
+
+  for (const n of nums) {
+    total += n;
+  }
+  return total;
+}
+
+const result = add(5, 10, 20);
+
+console.log(result);

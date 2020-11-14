f59e16277a9456f7c52b22496d21e02f3c1457cb
= <saleh.majeed8@gmail.com>
2020-11-14 17:31:06 +0530

    singleton classes

---
+++ js/singleton_classes.js
@@ -0,0 +1,20 @@
+const { time } = require("console");
+
+class Settings {
+  constructor() {
+    if (Settings.instance instanceof Settings) return Settings.instance;
+
+    this.settitng_object = {
+      background: "#ff0000",
+      varsion: Math.floor(Math.random() * 4000),
+    };
+
+    Object.freeze(this.settitng_object);
+    Object.freeze(this);
+    Settings.instance = this;
+  }
+
+  get(key) {
+    return this.settitng_object[key];
+  }
+}

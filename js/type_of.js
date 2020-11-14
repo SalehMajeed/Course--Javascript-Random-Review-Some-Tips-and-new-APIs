7af6ea6e2fdc032b3244a045652f695f0ceb6673
= <saleh.majeed8@gmail.com>
2020-11-14 10:17:48 +0530

    typeof

---
+++ js/type_of.js
@@ -0,0 +1,17 @@
+console.log(typeof "hello");
+console.log(typeof 123);
+console.log(typeof { name: "anony", age: 19 });
+console.log(typeof [1, 2, 3, 4]);
+console.log(typeof undefined);
+console.log(typeof null);
+
+let declared;
+
+console.log(typeof declared);
+console.log(typeof hello);
+
+class hi {}
+console.log(typeof hi);
+
+console.log(typeof foo);
+function foo() {}

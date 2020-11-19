5aa836c161ef7294c614882093390e716e1cf923
= <saleh.majeed8@gmail.com>
2020-11-19 21:11:56 +0530

    switch tab

---
+++ js/switch_tabs.js
@@ -0,0 +1,5 @@
+document.addEventListener("visibilitychange", () => {
+  //   document.title = document.visibilityState;
+  //   console.log(document.visibilityState);
+  console.log(document.hidden);
+});
